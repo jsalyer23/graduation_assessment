394f0e77c7e050bbf733b9f93806bfc288a28fac
Joshua Salyer <joshsalyer23@gmail.com>
2016-09-01 10:00:40 -0500

    Added controller actions

--- graduation_assessment/app/controllers/messages_controller.rb
+++ graduation_assessment/app/controllers/messages_controller.rb
@@ -1,2 +1,27 @@
 class MessagesController < ApplicationController
+
+	def show
+		@messages = Message.all
+	end
+
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.new(message_params)
+
+		if @message.save
+			flash[:notice] = "Thanks for adding a message!"
+			redirect_to messages_path
+		else
+			flash[:notice] = "Something went wrong when adding your new message..."
+			render :new
+		end
+	end
+
+	private
+	def message_params
+		params.require(:message).permit(:name, :body)
+	end
 end
